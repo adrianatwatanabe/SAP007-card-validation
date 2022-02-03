09c33ea071d44fcb3c6cfe5677e338b421c2cd56
adrianatwatanabe <adriana.t.watanabe@gmail.com>
2022-02-03 20:20:16 -0300

    Atualização da janela modal

--- src/index.js
+++ src/index.js
@@ -1,60 +1,31 @@
-//import validator from "./validator.js";
+import validator from "./validator.js";
 
-const reloadPage = document
-  .getElementById("logo")
-  .addEventListener("click", function () {
-    location.reload();
-  });
+document.getElementById("logo").addEventListener("click", function () {
+  location.reload();
+});
 
 initModalAbout();
 initModalContacts();
 
-/*
-
-
-PARA TESTAR SELETOR: console.log(contato.elements.teste)
-
-
-limpar entrada de número : tirar . e traço
-
-validator.isValid()
-
-validadtor.maskify
-
-*/
-
 function initModalAbout() {
-  const openModal = document.querySelector('[data-modal="openModalAbout"]');
-  const closeModal = document.querySelector('[data-modal="closeModalAbout"]');
-  const containerModal = document.querySelector(
+  let openModal = document.querySelector('[data-modal="openModalAbout"]');
+  let closeModal = document.querySelector('[data-modal="closeModalAbout"]');
+  let containerModal = document.querySelector(
     '[data-modal="containerModalAbout"]'
   );
-
-  if (openModal && closeModal && containerModal) {
-    function toggleModal(event) {
-      event.preventDefault();
-      containerModal.classList.toggle("ativo");
-    }
-    function clickOutsideModal(event) {
-      if (event.target === this) {
-        toggleModal(event);
-      }
-    }
-    openModal.addEventListener("click", toggleModal);
-    closeModal.addEventListener("click", toggleModal);
-    containerModal.addEventListener("click", clickOutsideModal);
-  }
+  analyzeModal(openModal, closeModal, containerModal);
 }
 
 function initModalContacts() {
-  const openModal = document.querySelector('[data-modal="openModalContacts"]');
-  const closeModal = document.querySelector(
-    '[data-modal="closeModalContacts"]'
-  );
-  const containerModal = document.querySelector(
+  let openModal = document.querySelector('[data-modal="openModalContacts"]');
+  let closeModal = document.querySelector('[data-modal="closeModalContacts"]');
+  let containerModal = document.querySelector(
     '[data-modal="containerModalContacts"]'
   );
+  analyzeModal(openModal, closeModal, containerModal);
+}
 
+function analyzeModal(openModal, closeModal, containerModal) {
   if (openModal && closeModal && containerModal) {
     function toggleModal(event) {
       event.preventDefault();
--- src/validator.js
+++ src/validator.js
@@ -1,23 +1,22 @@
-const validator = { //objeto
-
-  isValid(creditCardNumber){
-    /*
+const validator = {
+  //objeto
+  //limpar entrada de número : tirar . e traço
+  /*
     string com número de cartão a ser verificado
     retorna um boolean de acordo com algoritmo de Luhn
-    */
+  
   }
   
-  maskify(creditCardNumber){
-    /*
+
     string com número de cartão
     retorna string com os 4 últimos caracteres visíveis #
     manter os 4 caracteres intactos, mesmo quando a string for mais curta
-    */
-  }
+
+
   //de 11 a 16 dígitos
   // ...
 
-  /*
+
     Perguntar o número do cartão
     Passar os números em um array em ordem inverso
     Aplicar operações dos números das posições pares
