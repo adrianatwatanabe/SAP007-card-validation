d144785c910eb74c08674ba19bf15fa697bbd828
adrianatwatanabe <adriana.t.watanabe@gmail.com>
2022-02-03 18:04:18 -0300

    Modal funcionando

--- src/index.js
+++ src/index.js
@@ -1,3 +1,72 @@
-import validator from './validator.js';
+//import validator from "./validator.js";
 
-console.log(validator);
+const reloadPage = document
+  .getElementById("logo")
+  .addEventListener("click", function () {
+    location.reload();
+  });
+
+initModalAbout();
+initModalContacts();
+
+/*
+
+
+PARA TESTAR SELETOR: console.log(contato.elements.teste)
+
+
+limpar entrada de número : tirar . e traço
+
+validator.isValid()
+
+validadtor.maskify
+
+*/
+
+function initModalAbout() {
+  const openModal = document.querySelector('[data-modal="openModalAbout"]');
+  const closeModal = document.querySelector('[data-modal="closeModalAbout"]');
+  const containerModal = document.querySelector(
+    '[data-modal="containerModalAbout"]'
+  );
+
+  if (openModal && closeModal && containerModal) {
+    function toggleModal(event) {
+      event.preventDefault();
+      containerModal.classList.toggle("ativo");
+    }
+    function clickOutsideModal(event) {
+      if (event.target === this) {
+        toggleModal(event);
+      }
+    }
+    openModal.addEventListener("click", toggleModal);
+    closeModal.addEventListener("click", toggleModal);
+    containerModal.addEventListener("click", clickOutsideModal);
+  }
+}
+
+function initModalContacts() {
+  const openModal = document.querySelector('[data-modal="openModalContacts"]');
+  const closeModal = document.querySelector(
+    '[data-modal="closeModalContacts"]'
+  );
+  const containerModal = document.querySelector(
+    '[data-modal="containerModalContacts"]'
+  );
+
+  if (openModal && closeModal && containerModal) {
+    function toggleModal(event) {
+      event.preventDefault();
+      containerModal.classList.toggle("ativo");
+    }
+    function clickOutsideModal(event) {
+      if (event.target === this) {
+        toggleModal(event);
+      }
+    }
+    openModal.addEventListener("click", toggleModal);
+    closeModal.addEventListener("click", toggleModal);
+    containerModal.addEventListener("click", clickOutsideModal);
+  }
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,39 @@
-const validator = {
+const validator = { //objeto
+
+  isValid(creditCardNumber){
+    /*
+    string com número de cartão a ser verificado
+    retorna um boolean de acordo com algoritmo de Luhn
+    */
+  }
+  
+  maskify(creditCardNumber){
+    /*
+    string com número de cartão
+    retorna string com os 4 últimos caracteres visíveis #
+    manter os 4 caracteres intactos, mesmo quando a string for mais curta
+    */
+  }
+  //de 11 a 16 dígitos
   // ...
+
+  /*
+    Perguntar o número do cartão
+    Passar os números em um array em ordem inverso
+    Aplicar operações dos números das posições pares
+      . números na posição par * 2
+      . se o dobro >= 10, somar os dígitos -> 10 = 1 + 0 = 1
+      . Somar os resultados + números da posição ímpares
+    Comprovar se é válido
+      . Resultado da soma / 10 = 0 -> válido
+      
+
+    Retornar a resposta, limpando o input e mostrando o número do cartão em baixo, mascarado.
+
+    Array.prototype.join() : junta todos os elementos de um array em uma string e retorna esta string.
+
+    Array.prototype.reverse() : inverte os itens de um array. O primeiro elemento do array se torna o último e o último torna-se o primeiro.
+  */
 };
 
 export default validator;
